99c505c591b765048048f850c9b0590530424796
Mot0511 <suvorov.matvej9@gmail.com>
2024-06-25 10:45:47 +0300

    _

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from page_router import page_router
 from auth.router import auth_router
 from db.engine import create_db
+import uvicorn
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
@@ -14,3 +15,6 @@ app.include_router(page_router)
 @app.on_event("startup")
 async def startup_event():
     await create_db()
+
+if __name__ == "__main__":
+  uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
